828bb67d44a3ddd657fc80b9c4a520a20439701b
jlyharia <jlyhtech@gmail.com>
2018-11-30 23:10:06 -0800

    clean up

--- src/LeetCode.cpp
+++ src/LeetCode.cpp
@@ -6,56 +6,10 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <LeetCodeHeaders.hpp>
 
 using namespace std;
 
 int main() {
 
-	//	Matrix mx;
-	//	mx.test();
-
-	// StringQuiz obj;
-	// obj.test();
-
-	// Backtracking bt;
-	// bt.test();
-
-	// Trie tr;
-	// tr.test();
-	// LinkedList list;
-	// list.test();
-
-	// ArrayQuiz aq;
-	// aq.test();
-
-	//	Sorting st;
-	//	st.test();
-
-	// DynamicQuiz dq;
-	// dq.test();
-	// MinStack ms;
-	// ms.test();
-	//	Numbers nb;
-	//	nb.test();
-
-	// BitMagic bm;
-	// bm.test();
-
-	// LinkedList ll;
-	// ll.test();
-
-	// TreeQuiz btt;
-	// btt.test();
-
-	// Graph gp;
-	// gp.test();
-	//	MathQuiz mq;
-	//	mq.test();
-
-	//	MostFrequentElements me;
-	//	me.test();
-	MatrixQuiz mxQ;
-	mxQ.test();
-	return 0;
+    return 0;
 }
--- test/ArrayQuizTest.cpp
+++ test/ArrayQuizTest.cpp
@@ -3,27 +3,13 @@
 //
 
 #include "gtest/gtest.h"
-#include "LeetCodeHeaders.hpp"
+#include <iostream>
+#include <ArrayQuiz.hpp>
+
+using namespace std;
 
 ArrayQuiz aq;
 
 TEST (ArrayTest, findDuplicate) {
-    std::vector<int> vec{1, 2, 3, 4, 5, 6, 5, 7, 8, 9};
-    EXPECT_EQ(5, aq.findDuplicate(vec));
-}
-
-TEST (ArrayTest, lengthOfLongestSubstring) {
-    cout << "\nLongest Substring Without Repeating Characters\n";
-    string s1 = "abcabcbb";
-    EXPECT_EQ(3, aq.lengthOfLongestSubstring(s1));
-    string s2 = "bbbbb";
-    EXPECT_EQ(1, aq.lengthOfLongestSubstring(s2));
-    string s3 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\\\"#$%&'()*+,-./:;<=>?@[\\\\]^_`{|}~";
-    EXPECT_EQ(86, aq.lengthOfLongestSubstring(s3));
-}
-
-
-TEST (ArrayTest, TaskScheduler) {
-    std::vector<char> tasks = {'A', 'A', 'A', 'B', 'B', 'B'};
-    EXPECT_EQ(8, aq.leastInterval(tasks, 2));
+    cout<<"test"<<endl;
 }
