fa69d918a54f9dd61ab3fd93a7b35c0252a0d1b6
jlyharia <jlyhtech@gmail.com>
2018-12-01 23:56:49 -0800

    rever

--- src/leetcodeChallenge/LinkedListQuiz.cpp
+++ src/leetcodeChallenge/LinkedListQuiz.cpp
@@ -12,35 +12,20 @@
     92 + 965 = 1057
  */
 ListNode *LinkedListQuiz::addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode *dummy = new ListNode(0), *p = dummy;
     int carry = 0;
-    int res = 0;
-    int sum = 0;
-    ListNode *head = nullptr;
-    ListNode *prev = nullptr;
-
-    while (l1 != nullptr || l2 != nullptr) {
-
-        sum = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
-
-        carry = sum / 10;
-        res = sum % 10;
-
-        ListNode *temp = new ListNode(res);
-
-        if (head == nullptr) {
-            head = temp;
-        } else {
-            prev->next = temp;
+    while (l1 || l2 || carry) {
+        if (l1) {
+            carry += l1->val;
+            l1 = l1->next;
         }
-        prev = temp;
-
-        if (l1) l1 = l1->next;
-        if (l2) l2 = l2->next;
+        if (l2) {
+            carry += l2->val;
+            l2 = l2->next;
+        }
+        p->next = new ListNode(carry % 10);
+        carry /= 10;
+        p = p->next;
     }
-
-    if (carry > 0) {
-        prev->next = new ListNode(carry);
-    }
-
-    return head;    
+    return dummy->next;
 }
